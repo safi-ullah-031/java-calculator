6314aeb1e879c964840831350e16d8ca38bf4758
Safi Ullah <safiullah445@outlook.com>
2025-02-08 14:14:24 +0500

    Basic java Program for the calculator
    Basic java program which perform addition. subtraction, multiplication and division

---
+++ calculator.java
@@ -0,0 +1,26 @@
+public class calculator{
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 5;
+
+        // Addition
+        double sum = num1 + num2;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        double difference = num1 - num2;
+        System.out.println("Subtraction: " + difference);
+
+        // Multiplication
+        double product = num1 * num2;
+        System.out.println("Multiplication: " + product);
+
+        // Division
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.println("Division: " + quotient);
+        } else {
+            System.out.println("Division by zero is not allowed.");
+        }
+    }
+}
